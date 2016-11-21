63c055e9ce3f0883110ecc78a9bdf8b2598de6bd
Gustav Henning <nightmunnas@gmail.com>
2016-11-21 16:00:17 +0100

    os migration

--- ubuntu_clean_install.sh
+++ ubuntu_clean_install.sh
@@ -1,1 +1,32 @@
-# Script for a new setup (Ubuntu)
+# Script for a new setup (Ubuntu or Cygwin)
+
+RUNNING_CYGWIN=$(uname -o | grep "Cygwin" | wc -l)
+
+if [ $RUNNING_CYGWIN -eq 1 ]; then
+  # install apt-cyg if not installed
+  echo "cygwin prerequisites..."
+else
+  echo "linux prerequisites..."
+fi
+
+# sagi: (s)udo (a)pt-(g)et (i)nstall
+function sagi {
+  sudo apt-get --yes install $1
+}
+
+# jgi: (j)ust (g)et (i)nstall
+function jgi {
+  for var in "$@"
+  do
+    if [ $RUNNING_CYGWIN -eq 1 ]; then
+      apt-cyg install "$var"
+    else #linux
+      sagi "$var"
+    fi
+  done
+}
+
+
+# shared libraries
+jgi lynx make git nano subversion sed php perl mysql-common
+#TODO: Make, git, nano, subversion, sed, php, perl, mysql-common,
