4f08fd80bb66e433151d56bc53abab8cc2aa6caf
Gustav <nightmunnas@gmail.com>
2016-11-21 22:30:01 +0100

    added some more stuff, tested

--- ubuntu_clean_install.sh
+++ ubuntu_clean_install.sh
@@ -1,18 +1,25 @@
-PACKAGES="aptitude atom autogen automake autoconf arj \
+#!/bin/sh
+
+GIT_NAME="Gustav Henning"
+GIT_EMAIL="nightmunnas@gmail.com"
+
+PACKAGES="aptitude autogen automake autoconf atom arj \
 build-essential checkinstall calibre \
-dkpg deluge default-jre default-java-plugin \
+dpkg deluge default-jre default-java-plugin \
 faac faad ffmpeg flac \
 git gnome-shell gnome-tweak-tool gnome-mplayer geany gimp guake \
 htop inkscape lynx lame most mysql-common mpg123 \
 nano nmap nethogs php perl puddletag \
-sublime source-highlight subversion sed \
-unzip unrar ubuntu-restricted-extrasp7zip-full unetbootin \
+sublime-text-installer source-highlight subversion sed \
+unzip unrar p7zip-full unetbootin \
 vlc vorbis-tools wget xclip x264"
 
 # Script for a new setup (Ubuntu or Cygwin)
 RUNNING_CYGWIN=$(uname -o | grep "Cygwin" | wc -l)
 
+#
 # PRE INSTALL TODO
+#
 if [ $RUNNING_CYGWIN -eq 1 ]; then
   # install apt-cyg if not installed
   echo "cygwin prerequisites..."
@@ -25,15 +32,15 @@ fi
 # (j)ust (g)et (u)pdate
 function jgu {
     if [ $RUNNING_CYGWIN -eq 1 ]; then
-      apt-cyg update
+      apt-cyg update 1> /dev/null
     else
-      sudo apt-get update
+      sudo apt-get update 2>&1 /dev/null
     fi
 }
 
 # sagi: (s)udo (a)pt-(g)et (i)nstall
 function sagi {
-  sudo apt-get --yes install $1
+  sudo apt-get -q --yes install $1
 }
 
 # jgi: (j)ust (g)et (i)nstall
@@ -43,15 +50,22 @@ function jgi {
     if [ $RUNNING_CYGWIN -eq 1 ]; then
       ALRDY=$(apt-cyg | grep "^$var$" | wc -l)
       if [ $ALRDY -eq 0 ]; then
-        apt-cyg install "$var" 2> /dev/null # edit this for extensive info
+        apt-cyg install "$var" 1> /dev/null 
       fi
     else #linux
-      sagi "$var"
+	sagi "$var" 1> /dev/null
+	echo -n "."
     fi
   done
+  
 }
 
+#
 # PACKAGES FROM *-GET
+#
+sudo add-apt-repository --yes ppa:webupd8team/atom #Atom
+sudo add-apt-repository --yes ppa:webupd8team/sublime-text-3 #Sublime
+
 jgu
 jgi $PACKAGES
 
@@ -77,17 +91,38 @@ function ubuntu_manual {
   sudo apt-get remove -y --purge totem
 
 ##
+## Foxit Reader for PDFs
+## TODO: MAke it grab latest version
+
+wget http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.2/en_us/FoxitReader2.2.1025_Server_x64_enu_Setup.run.tar.gz
+tar xzvf FoxitReader*.tar.gz
+sudo chmod a+x FoxitReader*.run
+./FoxitReader.*.run
+sudo ./FoxitReader.*.run
+
+
+
+##
 ## And to finish it, a dist-upgrade to install/update them all.
 ##
   sudo apt-get update && sudo apt-get dist-upgrade -y
 
 }
 
+#
 # post install
+#
 if [ $RUNNING_CYGWIN -eq 1 ]; then
-
+	echo "cygwin post"
 else
   ubuntu_manual
 fi
 
+#
+# Git config
+#
+git config --global user.name $GIT_NAME
+git config --global user.email $GIT_EMAIL
+
 echo "Have fun"
+
