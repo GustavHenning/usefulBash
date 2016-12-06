bdacb031ecb2c2dacf7d44867d078984e4bcb235
Gustav <nightmunnas@gmail.com>
2016-12-06 10:23:44 +0100

    improved makePDF

--- latex/makePDF.sh
+++ latex/makePDF.sh
@@ -7,5 +7,10 @@ fi
 
 pdflatex thesis.tex
 
-mv ./*.aux ./out
+#mv ./*.aux ./out
 mv ./*.out ./out
+
+if [ $(ps -ef | grep FoxitReader | grep -v grep | wc -l) -gt 0 ]; then
+	kill $(pidof FoxitReader)
+fi
+~/opt/foxitsoftware/foxitreader/FoxitReader.sh ./thesis.pdf > /dev/null 2>&1 &
